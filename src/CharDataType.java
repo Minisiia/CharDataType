751047c31eefd1cae7120a6b6063022dfbb2a844
Minisiia <ar_tik@yahoo.com>
2022-11-20 16:11:39 +0200

    Java Start. 2. Machine mathematics. Data types. Char Data Type

---
+++ src/CharDataType.java
@@ -0,0 +1,50 @@
+public class CharDataType {
+    public static void main(String[] args) {
+        char c1 = '\u0414';
+        char c2 = '\u043E';
+        char c3 = '\u0431';
+        char c4 = '\u0440';
+        char c5 = '\u0438';
+        char c6 = '\u0434';
+        char c7 = '\u0435';
+        char c8 = '\u043D';
+        char c9 = '\u044C';
+        char c10 = '\u002C';
+        char c11 = '\u0020';
+        char c12 = '\u0410';
+        char c13 = '\u043D';
+        char c14 = '\u0430';
+        char c15 = '\u0441';
+        char c16 = '\u0442';
+        char c17 = '\u0430';
+        char c18 = '\u0441';
+        char c19 = '\u0456';
+        char c20 = '\u0454';
+        char c21 = '\u0021';
+
+        System.out.println(""+c1+c2+c3+c4+c5+c6+c7+c8+c9+c10+c11+c12+c13+c14+c15+c16+c17+c18+c19+c20+c21);
+        // або
+        System.out.println(Character.toString(c1)+
+                Character.toString(c2)+
+                Character.toString(c3)+
+                Character.toString(c4)+
+                Character.toString(c5)+
+                Character.toString(c6)+
+                Character.toString(c7)+
+                Character.toString(c8)+
+                Character.toString(c9)+
+                Character.toString(c10)+
+                Character.toString(c11)+
+                Character.toString(c12)+
+                Character.toString(c13)+
+                Character.toString(c14)+
+                Character.toString(c15)+
+                Character.toString(c16)+
+                Character.toString(c17)+
+                Character.toString(c18)+
+                Character.toString(c19)+
+                Character.toString(c20)+
+                Character.toString(c21));
+
+    }
+}
